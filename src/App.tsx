2cb683d8b23a03c54b6ce5a8dbcc77965845e3ab
shafiq <shafiq@gmail.com>
2024-04-16 11:52:21 +0300

    mini

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,14 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<Signup_page />} />
         <Route path='/login' element={<Signin_page />} />
-        <Route path='/chartroom' element={<Chat_room socket={socket} />} />
+        <Route
+          path='/chartroom'
+          element={
+            <Protected>
+              <Chat_room socket={socket} />
+            </Protected>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
